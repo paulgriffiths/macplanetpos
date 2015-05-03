baf31039209cd8fd5b7e3d29fd1ef306652a842b
Paul Griffiths <mail@paulgriffiths.net>
2015-05-03 17:30:28 -0400

    Initial Commit

---
+++ MacAstro/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MacAstro
+//
+//  Created by Paul Griffiths on 5/3/15.
+//  Copyright (c) 2015 Paul Griffiths. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
